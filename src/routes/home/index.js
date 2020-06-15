8e1fb0421517df7fb19b47182036680a03673d2f
Madi Yessirkepov <madi.yessirkepov@tado.com>
2020-06-16 01:23:41 +0200

    WIP

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -2,37 +2,35 @@ import { h } from "preact";
 import style from "./style";
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const cardRotations = {
-  1: -25,
-  2: -20,
-  3: -15,
-  4: -10,
-  5: -5,
-  6: 5,
-  7: 10,
-  8: 15,
-  9: 20,
-  10: 25,
-};
+// dynamic
+const rate = 4;
+const firstElementDegree = -(cards.length % 2 == 0
+  ? 2.5 + rate * (cards.length / 2 - 1)
+  : rate * Math.floor(cards.length / 2));
 
-const Home = () => (
-  <div class={style.home}>
-    {cards.map((card, index) => (
-      <div
-        class={style.card}
-        style={{
-          transform: `rotate(${cardRotations[index + 1]}deg)`,
-          transformOrigin: "center bottom",
-          marginRight: -20 + "px",
-          color: "white",
-          marginTop: 2 * Math.abs(cardRotations[index + 1]) + "px",
-          backgroundColor: "#" + Math.floor(Math.random() * 16777215).toString(16),
-        }}
-      >
-        {card}
-      </div>
-    ))}
-  </div>
-);
+const cardRotations = cards.map((card, index) => firstElementDegree + index * rate);
+
+console.log(firstElementDegree, cardRotations);
+const Home = () => {
+  return (
+    <div class={style.home}>
+      {cards.map((card, index) => (
+        <div
+          class={style.card}
+          style={{
+            transform: `rotate(${cardRotations[index]}deg)`,
+            transformOrigin: "center bottom",
+            marginRight: `${-30}px`, // dynamic
+            color: "white",
+            marginTop: `${Math.abs(cardRotations[index])}px`,
+            backgroundColor: `#${Math.floor(Math.random() * 16777215).toString(16)}`,
+          }}
+        >
+          {card}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Home;
