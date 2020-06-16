7441d0358b2cb3b169c30a892e056132b5c507a4
Daniel Benko <madi.yessirkepov@tado.com>
2020-06-16 02:04:21 +0200

    magiiiic

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,47 +1,70 @@
 import { h } from "preact";
 import style from "./style";
 
-const colors = [
-  "#5c1798",
-  "#b440a3",
-  "#fc9d6d",
-  "#bb32e7",
-  "#96e1d5",
-  "#ec308c",
-  "#fc9d6d",
-  "#eef399",
-  "#3f9399",
-  "#60fecb",
-];
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const rate = 0.5 * cards.length;
-
-const firstElementDegree = -(cards.length % 2 == 0
-  ? rate / 2 + rate * (cards.length / 2 - 1)
-  : rate * Math.floor(cards.length / 2));
-
-const cardRotations = cards.map((card, index) => firstElementDegree + index * rate);
-
-console.log(firstElementDegree, cardRotations);
-const Home = () => {
-  return (
-    <div class={style.home}>
-      {cards.map((card, index) => (
-        <div
-          class={style.card}
-          style={{
-            transform: `rotate(${cardRotations[index]}deg) translate(0, ${Math.abs(
-              cardRotations[index] * (index == 0 || index == cards.length - 1 ? 1.25 : 1)
-            )}px)`,
-
-            backgroundColor: colors[index],
-          }}
-        >
-          {card}
-        </div>
-      ))}
-    </div>
-  );
+const cardRotations = {
+  1: -25,
+  2: -20,
+  3: -15,
+  4: -10,
+  5: -5,
+  6: 5,
+  7: 10,
+  8: 15,
+  9: 20,
+  10: 25,
 };
+
+let R = 800;
+let S = 30;
+let phi = 0;
+let phi2 = 0;
+let i = 0;
+let angles = [];
+let X = [];
+let segments = 320;
+let numCards = 10;
+const cards = [];
+for (let j = 0; j < numCards; ++j) cards.push(j);
+
+for (let j = 0; j < segments; ++j) {
+  X[j] = {
+    x: R * Math.sin((getIndex(j) / segments) * 2 * Math.PI),
+    y: -R * Math.cos((getIndex(j) / segments) * 2 * Math.PI),
+  };
+}
+
+function getIndex(i) {
+  return 2 * (i - Math.floor(numCards / 2)) + 1 - (numCards % 2);
+}
+
+console.log(X);
+while (phi < 2 * Math.PI) {
+  angles[i] = phi;
+  phi += 2 * Math.asin(S / 2 / R);
+  i++;
+}
+
+const Home = () => (
+  <div class={style.home}>
+    {cards.map((card, index) => (
+      <div
+        class={style.card}
+        style={{
+          position: "absolute",
+          top: "800px",
+          left: "800px",
+          transform: `translate(${X[index].x}px,${X[index].y}px) 
+             rotate(${(getIndex(index) / segments) * 2 * Math.PI}rad) `,
+          marginRight: -20 + "px",
+          color: "white",
+          //marginTop: 2 * Math.abs(cardRotations[index + 1]) + "px",
+          backgroundColor: "#" + Math.floor(Math.random() * 16777215).toString(16),
+        }}
+      >
+        {card}
+      </div>
+    ))}
+  </div>
+);
 
 export default Home;
